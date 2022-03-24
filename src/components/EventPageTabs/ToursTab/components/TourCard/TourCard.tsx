78b8bcb935077300415f28f434db726835d2e8b3
Pavel Pozdnyakov <pavel.pozdnyakov@quantumsoft.ru>
2022-03-24 14:57:57 +0300

    fixes

--- src/components/EventPageTabs/ToursTab/components/TourCard/TourCard.tsx
+++ src/components/EventPageTabs/ToursTab/components/TourCard/TourCard.tsx
@@ -6,17 +6,19 @@ import styles from './TourCard.module.css';
 export const TourCard: Component<{ tour: TourInterface, isLast: boolean }> = (props) => {
   const [local] = splitProps(props, ['tour', 'isLast'])
   const [getModalActive, setModalActive] = createSignal(false)
-
+  const isMobile = window.innerWidth / window.innerHeight < 0.75
+  const dataAos = isMobile ? '' : 'fade-up'
+  
   return (
-    <div class={`${styles.tour} ${local.isLast && styles.noBorder}`} onclick={() => setModalActive(true)} data-aos="fade-up">
+    <div class={`${styles.tour} ${local.isLast && styles.noBorder}`} onclick={() => setModalActive(true)} data-aos={dataAos}>
       <Show when={getModalActive()}>
         <ToursModal text={local.tour.fullDescription} image={local.tour.bigImage} closeCard={() => setModalActive(false)} />
       </Show>
-      <img class={styles.img} src={local.tour.image} alt='' data-aos="fade-up" />
+      <img class={styles.img} src={local.tour.image} alt='' data-aos={dataAos} />
       <div class={styles.descriptionContainer}>
-        <span class={styles.date} data-aos="fade-up">{local.tour.date}</span>
-        <span class={styles.title} data-aos="fade-up">{local.tour.title}</span>
-        <span class={styles.description} data-aos="fade-up">{local.tour.description}</span>
+        <span class={styles.date} data-aos={dataAos}>{local.tour.date}</span>
+        <span class={styles.title} data-aos={dataAos}>{local.tour.title}</span>
+        <span class={styles.description} data-aos={dataAos}>{local.tour.description}</span>
       </div>
     </div>
   );
--- src/components/EventPageTabs/ToursTab/components/ToursYear/ToursYear.tsx
+++ src/components/EventPageTabs/ToursTab/components/ToursYear/ToursYear.tsx
@@ -5,9 +5,11 @@ import styles from './ToursYear.module.css';
 
 export const ToursYear: Component<{ tours: TourInterface[], isFirst: boolean}> = (props) => {
   const [local] = splitProps(props, ['tours', 'isFirst'])
+  const isMobile = window.innerWidth / window.innerHeight < 0.75
+  const dataAos = isMobile ? '' : 'fade-up'
   return (
     <div class={`${styles.toursContainer} ${local.isFirst && styles.firstContainer}`}>
-      <div class={styles.title} data-aos="fade-up">{Object.keys(local.tours)[0]}</div>
+      <div class={styles.title} data-aos={dataAos}>{Object.keys(local.tours)[0]}</div>
       <For each={Object.values(local.tours)[0]}>
         {(tour, idx) => <TourCard tour={tour as TourInterface} isLast={idx() + 1 === Object.values(local.tours)[0].length} />}
       </For>
